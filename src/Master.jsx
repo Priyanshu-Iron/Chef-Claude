147000cfaab9b642b9d2a2935e25917799d7b43f
Priyanshu Singh <priyanshu@Priyanshus-MacBook-Air.local>
2025-01-10 11:34:53 +0530

    Conditional Rendering Added

--- src/Master.jsx
+++ src/Master.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Master() {
+export default function Main() {
 
     const [ingredients, setIngredients] = React.useState([])
 
@@ -8,14 +8,14 @@ export default function Master() {
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function addIngredients(formData) {
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
         <main>
-            <form action={addIngredients} className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
@@ -24,9 +24,17 @@ export default function Master() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            {ingredients.length > 0 ? <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 ? <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div> : null}
+            </section> : null}
         </main>
     )
 }
